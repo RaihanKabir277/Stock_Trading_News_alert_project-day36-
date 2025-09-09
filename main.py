edbe7b77335e8b2385077a9bb9ad5b65ca20ef0f
RaihanKabir277 <raihank0198@gmail.com>
2025-09-09 12:56:32 +0600

    yesterday closing data fetching dynamically added here

--- main.py
+++ main.py
@@ -16,6 +16,13 @@ stock_params = {
 #--------- fetch the daily stock data ----------
 response = requests.get(url=STOCK_ENDPOINT,params=stock_params)
 response.raise_for_status()
-stock_data = response.json()
-print(stock_data)
+# stock_data = response.json()
+# print(stock_data)
+# stock_closing_data = stock_data["Time Series (Daily)"]["2025-09-08"]["4. close"]  #"2025-09-08" its a dictioanry in the json file so make it list for dynamically get data in previous day
+# print(stock_closing_data)
+stock_data = response.json()["Time Series (Daily)"]
+stock_list = [value for (key, value) in stock_data.items()]   #list comprehension
+# print(stock_list)
+yesterday_closing_stock_data = stock_list[0]["4. close"]
+print(yesterday_closing_stock_data)
 
